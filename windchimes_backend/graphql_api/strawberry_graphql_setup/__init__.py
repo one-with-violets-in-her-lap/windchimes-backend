3cf220add41e82c8c881b617592724dabb62dcc8
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-09-13 12:08:18 +0500

    graphql api security: enable schema introspection back

--- windchimes_backend/graphql_api/strawberry_graphql_setup/__init__.py
+++ windchimes_backend/graphql_api/strawberry_graphql_setup/__init__.py
@@ -1,7 +1,6 @@
 import strawberry
 from strawberry.fastapi import GraphQLRouter
-from strawberry.extensions import MaxAliasesLimiter, MaxTokensLimiter, AddValidationRules
-from graphql import NoSchemaIntrospectionCustomRule
+from strawberry.extensions import MaxAliasesLimiter, MaxTokensLimiter
 
 from windchimes_backend.core.config import app_config
 from windchimes_backend.graphql_api.strawberry_graphql_setup.context import (
@@ -10,10 +9,10 @@ from windchimes_backend.graphql_api.strawberry_graphql_setup.context import (
 from windchimes_backend.graphql_api.mutations import Mutation
 from windchimes_backend.graphql_api.queries import Query
 
+
 security_extensions = []
 if app_config.mode == "PROD":
     security_extensions = [
-        AddValidationRules([NoSchemaIntrospectionCustomRule]),
         MaxAliasesLimiter(max_alias_count=15),
         MaxTokensLimiter(max_token_count=1000),
     ]
