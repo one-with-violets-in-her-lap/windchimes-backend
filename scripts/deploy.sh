74c54cb48412f622e60f98fe7429db6f61bf558a
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-09-13 12:08:18 +0500

    deploy script: add `--force` option to docker container prune command to skip confirmation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ read -p "branch to deploy (main by default): " branch
 # set branch to `main` on empty input
 branch=${branch:-main}
 
-sshpass -p $SSH_PASSWORD ssh -l root $SSH_ADDRESS "cd ~/projects/windchimes-backend && git remote set-url origin $GIT_ORIGIN && git fetch && git switch $branch && git pull origin $branch && docker compose down --rmi all && docker container prune && docker compose up -d"
+sshpass -p $SSH_PASSWORD ssh -l root $SSH_ADDRESS "cd ~/projects/windchimes-backend && git remote set-url origin $GIT_ORIGIN && git fetch && git switch $branch && git pull origin $branch && docker compose down --rmi all && docker container --force && docker compose up -d"
