2957fe02503b5505ffb690872a105676379fb517
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-09-13 12:08:19 +0500

    implement youtube m3u8 playing

--- windchimes/api/main.py
+++ windchimes/api/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from windchimes.api.audio_proxy import audio_proxy_router
 from windchimes.logging_setup import root_logger
 from windchimes.core.config import app_config
 from windchimes.api.lifespan import lifespan
@@ -12,6 +13,8 @@ root_logger.info("Launching uvicorn serving Graphql API")
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(graphql_router, prefix="/graphql")
+app.include_router(audio_proxy_router)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=app_config.api.cors_allowed_origins,
--- windchimes/api/queries/tracks/track_audio_file_query.py
+++ windchimes/api/queries/tracks/track_audio_file_query.py
@@ -1,10 +1,16 @@
-from typing import Optional
+from typing import Optional, cast
 
+from fastapi import Request
 import strawberry
 
 from windchimes.core.models.platform import Platform
 from windchimes.api.reusable_schemas.errors import GraphQLApiError
 from windchimes.api.utils.graphql import GraphQLRequestInfo
+from windchimes.api.audio_proxy import audio_proxy_router
+
+
+PLATFORMS_TO_PROXY = [Platform.YOUTUBE]
+"""Platforms that don't support CORS requests, so a CORS proxy is needed"""
 
 
 @strawberry.type
@@ -28,6 +34,11 @@ async def _get_track_audio_file(
 
     if audio_file_url is None:
         return None
+
+    if platform in PLATFORMS_TO_PROXY:
+        current_api_url = cast(Request, info.context.request).base_url
+        proxy_url = current_api_url.replace(path=audio_proxy_router.prefix)
+        audio_file_url = str(proxy_url.include_query_params(url=audio_file_url))
 
     return TrackAudioFileGraphQL(url=audio_file_url)
 
