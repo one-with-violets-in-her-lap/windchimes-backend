d24f418db1d61df1c073f522ff7967e15feb6a1b
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-09-13 12:08:17 +0500

    logging: enhance formatting

--- windchimes_backend/logging_setup.py
+++ windchimes_backend/logging_setup.py
@@ -1,5 +1,10 @@
 import logging
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s: [%(levelname)s] %(name)s - %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
+
 root_logger = logging.getLogger()
