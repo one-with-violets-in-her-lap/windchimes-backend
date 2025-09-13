6643384c3b9c2c45260b934555cefaed25b2e603
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-09-13 12:08:18 +0500

    graphql api security middlewares: fix schema introspection disabling

--- windchimes_backend/graphql_api/strawberry_graphql_setup/__init__.py
+++ windchimes_backend/graphql_api/strawberry_graphql_setup/__init__.py
@@ -1,7 +1,7 @@
 import strawberry
 from strawberry.fastapi import GraphQLRouter
-from strawberry.extensions import MaxAliasesLimiter, MaxTokensLimiter
-from graphql import NoDeprecatedCustomRule, NoSchemaIntrospectionCustomRule
+from strawberry.extensions import MaxAliasesLimiter, MaxTokensLimiter, AddValidationRules
+from graphql import NoSchemaIntrospectionCustomRule
 
 from windchimes_backend.core.config import app_config
 from windchimes_backend.graphql_api.strawberry_graphql_setup.context import (
@@ -13,8 +13,7 @@ from windchimes_backend.graphql_api.queries import Query
 security_extensions = []
 if app_config.mode == "PROD":
     security_extensions = [
-        NoSchemaIntrospectionCustomRule,
-        NoDeprecatedCustomRule,
+        AddValidationRules([NoSchemaIntrospectionCustomRule]),
         MaxAliasesLimiter(max_alias_count=15),
         MaxTokensLimiter(max_token_count=1000),
     ]
