d0594a5d895b7eada907234004bba10ba60375c0
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-09-13 12:08:18 +0500

    add cors middleware

--- windchimes_backend/graphql_api/main.py
+++ windchimes_backend/graphql_api/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from windchimes_backend.logging_setup import root_logger
 from windchimes_backend.config import app_config
@@ -11,5 +12,11 @@ root_logger.info("Launching uvicorn serving Graphql API")
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(graphql_router, prefix="/graphql")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=app_config.graphql_api.cors_allowed_origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 uvicorn.run(app, port=app_config.graphql_api.port)
